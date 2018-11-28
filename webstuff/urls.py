9e68bdbcf36bfb4a71edbd1729e78e42b51e02c8
Volyando Belvadra <belgendo@gmail.com>
2018-11-29 04:06:10 +0700

    belom

--- webstuff/urls.py
+++ webstuff/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.index, name='index')
-    #path('form/', views.get_name, name='get_name')
-    
+    path(r'', views.index, name='index'),
+    path(r'form/', views.calculate, name='calculate')  
 ]
